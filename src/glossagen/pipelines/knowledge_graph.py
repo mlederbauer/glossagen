9c0a6e6e35bd24c89cd65a6b534037fde0ea102a
Magdalena Lederbauer <mlederbauer@ethz.ch>
2024-05-20 23:08:17 +0200

    feat: barebone structure of KG

---
+++ src/glossagen/pipelines/knowledge_graph.py
@@ -0,0 +1,20 @@
+"""Module for generating a knowledge graph out of a document."""
+
+
+def generate_ontology_from_glossary():
+    """Pass for now."""
+    pass
+
+
+def generate_knowledge_graph():
+    """Pass for now."""
+    pass
+
+
+def main():
+    """Pass for now."""
+    pass
+
+
+if __name__ == "__main__":
+    main()
